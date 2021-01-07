2e840dcdf335df9d1a7b4d9d7786989fff80ede8
Christopher Banas <cjb5537@rit.edu>
2021-01-07 14:00:32 -0500

    Added names

--- src/model/Player/Position.java
+++ src/model/Player/Position.java
@@ -1,3 +1,7 @@
 package model.Player;
 
+/**
+ * Desc: Enum for what position a player is
+ * Author: Christopher Banas
+ */
 public enum Position {POINT_GUARD, SHOOTING_GUARD, SMALL_FORWARD, POWER_FORWARD, CENTER}
--- src/model/Player/Role.java
+++ src/model/Player/Role.java
@@ -4,4 +4,4 @@ package model.Player;
  * Desc: Enum for what role a player is
  * Author: Christopher Banas
  */
-public enum Role {BENCH, SIXTH_MAN, STARTER, STAR}
+public enum Role {STAR, STARTER, SIXTH_MAN, BENCH}
