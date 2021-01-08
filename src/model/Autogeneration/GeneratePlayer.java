76402cb73ff6fce60cc1cefbdcd28629cc07268d
Christopher Banas <cjb5537@rit.edu>
2021-01-08 17:49:27 -0500

    Generate player implemented

--- src/model/Autogeneration/GeneratePlayer.java
+++ src/model/Autogeneration/GeneratePlayer.java
@@ -3,6 +3,7 @@ package model.Autogeneration;
 import model.Autogeneration.data.NameLists;
 import model.Player.Player;
 import model.Player.Position;
+import model.Player.Role;
 import model.Player.Type;
 
 import java.util.HashSet;
@@ -33,22 +34,120 @@ public class GeneratePlayer {
         return name;
     }
 
-    public Type generateType(){
-        int numberOfTypes = 9; //not including god type and starting at 0
-        //TODO CHANGE
-        int random = new Random().nextInt(numberOfTypes);
+    public Type pointGuardType(Role role){
+        int random;
+        if(role.equals(Role.SUPERSTAR)){
+            random = (int) (Math.random() * (9 - 5 + 1) + 5); //superstar types from range 5-9
+        } else{
+            random = (int) (Math.random() * (4 + 1)); //star to bench types from range 0-4
+        }
+        return switch (random){
+            case 0 -> Type.PLAYMAKER;
+            case 1 -> Type.ALL_AROUND;
+            case 2 -> Type.OFFENSIVE_PLAYMAKER;
+            case 3 -> Type.DEFENSIVE_PLAYMAKER;
+            case 4 -> Type.PERIMETER_DEFENDER;
+            case 5 -> Type.OFFENSIVE_SUPERSTAR; //superstar only types from range 5-9
+            case 6 -> Type.POINT_GOD;
+            case 7 -> Type.BALANCED_SUPERSTAR;
+            case 8 -> Type.LOCKDOWN_DEFENDER;
+            case 9 -> Type.TWO_WAY_SUPERSTAR;
+            default -> throw new IllegalStateException("Unexpected value: " + random);
+        };
+    }
+
+    public Type shootingGuardType(Role role){
+        int random;
+        if(role.equals(Role.SUPERSTAR)){
+            random = (int) (Math.random() * (9 - 6 + 1) + 6); //superstar types from range 6-9
+        } else{
+            random = (int) (Math.random() * (5 + 1)); //star to bench types from range 0-5
+        }
         return switch (random){
             case 0 -> Type.THREE_AND_D;
             case 1 -> Type.ALL_AROUND;
-            case 2 -> Type.PAINT_BEAST;
-            case 3 -> Type.OFFENSIVE_PLAYMAKER;
-            case 4 -> Type.DEFENSIVE_PLAYMAKER;
-            case 5 -> Type.ENERGETIC_REBOUNDER;
-            case 6 -> Type.THREE_LEVEL_SCORER;
-            case 7 -> Type.POST_PLAYER;
-            case 8 -> Type.PERIMETER_DEFENDER;
-            case 9 -> Type.PURE_SHOOTER;
+            case 2 -> Type.THREE_LEVEL_SCORER;
+            case 3 -> Type.PERIMETER_DEFENDER;
+            case 4 -> Type.INSIDE_OUTSIDE_DEFENDER;
+            case 5 -> Type.PURE_SHOOTER;
+            case 6 -> Type.OFFENSIVE_SUPERSTAR; //superstar only types from range 6-9
+            case 7 -> Type.BALANCED_SUPERSTAR;
+            case 8 -> Type.LOCKDOWN_DEFENDER;
+            case 9 -> Type.TWO_WAY_SUPERSTAR;
             default -> throw new IllegalStateException("Unexpected value: " + random);
+        };
+    }
+
+    public Type smallForwardType(Role role){
+        int random;
+        if(role.equals(Role.SUPERSTAR)){
+            random = (int) (Math.random() * (10 - 7 + 1) + 7); //superstar types from range 7-10
+        } else{
+            random = (int) (Math.random() * (6 + 1)); //star to bench types from range 0-6
+        }
+        return switch (random){
+            case 0 -> Type.THREE_AND_D;
+            case 1 -> Type.ALL_AROUND;
+            case 2 -> Type.THREE_LEVEL_SCORER;
+            case 3 -> Type.PERIMETER_DEFENDER;
+            case 4 -> Type.INSIDE_OUTSIDE_DEFENDER;
+            case 5 -> Type.INSIDE_DEFENDER;
+            case 6 -> Type.PURE_SHOOTER;
+            case 7 -> Type.BALANCED_SUPERSTAR; //superstar only types from range 7-10
+            case 8 -> Type.LOCKDOWN_DEFENDER;
+            case 9 -> Type.TWO_WAY_SUPERSTAR;
+            case 10 -> Type.OFFENSIVE_SUPERSTAR;
+            default -> throw new IllegalStateException("Unexpected value: " + random);
+        };
+    }
+
+    public Type powerForwardType(Role role){
+        int random;
+        if(role.equals(Role.SUPERSTAR)){
+            random = (int) (Math.random() * (10 - 8 + 1) + 8); //superstar types from range 8-10
+        } else{
+            random = (int) (Math.random() * (7 + 1)); //star to bench types from range 0-7
+        }
+        return switch (random){
+            case 0 -> Type.ENERGETIC_REBOUNDER;
+            case 1 -> Type.POST_PLAYER;
+            case 2 -> Type.INSIDE_OUTSIDE_DEFENDER;
+            case 3 -> Type.PERIMETER_DEFENDER;
+            case 4 -> Type.INSIDE_DEFENDER;
+            case 5 -> Type.PURE_SHOOTER;
+            case 6 -> Type.STRETCH_BIG;
+            case 7 -> Type.THREE_LEVEL_SCORER;
+            case 8 -> Type.PAINT_BEAST; //superstar only types from range 8-10
+            case 9 -> Type.LOCKDOWN_DEFENDER;
+            case 10 -> Type.TWO_WAY_SUPERSTAR;
+            default -> throw new IllegalStateException("Unexpected value: " + random);
+        };
+    }
+
+    public Type centerType(Role role){
+        int random;
+        if(role.equals(Role.SUPERSTAR)){
+            random = 4;
+        } else{
+            random = (int) (Math.random() * (3 + 1)); //star to bench types from range 0-3
+        }
+        return switch (random){
+            case 0 -> Type.ENERGETIC_REBOUNDER;
+            case 1 -> Type.POST_PLAYER;
+            case 2 -> Type.INSIDE_DEFENDER;
+            case 3 -> Type.STRETCH_BIG;
+            case 4 -> Type.PAINT_BEAST; //superstar only
+            default -> throw new IllegalStateException("Unexpected value: " + random);
+        };
+    }
+
+    public Type generateType(Position position, Role role){
+        return switch (position){
+            case POINT_GUARD -> pointGuardType(role);
+            case SHOOTING_GUARD -> shootingGuardType(role);
+            case SMALL_FORWARD -> smallForwardType(role);
+            case POWER_FORWARD -> powerForwardType(role);
+            case CENTER -> centerType(role);
         };
     }
 
@@ -63,6 +162,70 @@ public class GeneratePlayer {
             case 4 -> Position.CENTER;
             default -> throw new IllegalStateException("Unexpected value: " + random);
         };
+    }
+
+    public Role generateRole(){
+        int random = new Random().nextInt(240);
+        if(random <= 20){ //0-20
+            return Role.SUPERSTAR;
+        } else if(random <= 60){ //21-60
+            return Role.STAR;
+        } else if(random <= 120){ //61-120
+            return Role.STARTER;
+        } else if(random <= 180){ //121-180
+            return Role.SIXTH_MAN;
+        } else{ //181-240
+            return Role.BENCH;
+        }
+    }
+
+    public int generateAge(){
+        return (int) (Math.random() * (22 - 19 + 1) + 19);
+    }
+
+    public Player createPlayer(){
+        Position position = generatePosition();
+        Role role = generateRole();
+        Type type = generateType(position, role);
+        return new Player(generateName(), generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(String name){
+        Position position = generatePosition();
+        Role role = generateRole();
+        Type type = generateType(position, role);
+        return new Player(name, generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(Position position){
+        Role role = generateRole();
+        Type type = generateType(position, role);
+        return new Player(generateName(), generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(Role role){
+        Position position = generatePosition();
+        Type type = generateType(position, role);
+        return new Player(generateName(), generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(Type type){
+        Position position = generatePosition();
+        Role role = generateRole();
+        return new Player(generateName(), generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(Position position, Role role){
+        Type type = generateType(position, role);
+        return new Player(generateName(), generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(Position position, Role role, Type type){
+        return new Player(generateName(), generateAge(), type, role, position);
+    }
+
+    public Player createPlayer(String name, int age, Position position, Role role, Type type){
+        return new Player(name, age, type, role, position);
     }
 
 }
--- src/tests/GeneratePlayerTest.java
+++ src/tests/GeneratePlayerTest.java
@@ -1,16 +1,35 @@
 package tests;
 
 import model.Autogeneration.GeneratePlayer;
+import model.Player.Player;
+import model.Player.Position;
+import model.Player.Role;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
 class GeneratePlayerTest {
+    GeneratePlayer generator;
+
+    @BeforeEach
+    void setUp(){
+        generator = new GeneratePlayer();
+    }
 
     @Test
-    void GeneratePlayer(){
-        GeneratePlayer generatePlayer = new GeneratePlayer();
-        System.out.println(generatePlayer.generateName());
-        System.out.println(generatePlayer.generateType());
-        System.out.println(generatePlayer.generatePosition());
+    void createPlayer(){
+        for(int i = 0; i<10; i++){
+            Player player = generator.createPlayer();
+            assertAll(() -> assertNotNull(player.getName()),
+                    () -> assertNotNull(player.getAge()),
+                    () -> assertNotNull(player.getPosition()),
+                    () -> assertNotNull(player.getRating()),
+                    () -> assertNotNull(player.getType()),
+                    () -> assertNotNull(player.getRole()));
+        }
     }
+
 
 }
